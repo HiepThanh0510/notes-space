505eaafa3c84c7bbcf19eac79c12eacdad46b7ee
HiepThanh0510 <vohiepthanhly1619@gmail.com>
2026-01-17 21:00:29 +0700

    update .gitignore

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "HiepThanh0510.github.io/notes-space",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "work", "templates", ".obsidian", "drafts", "**/README.md", "**/_template.md", "**/BLOGGING-GUIDE.md"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
